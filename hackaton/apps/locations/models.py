c4618c6e4a129f7a76c8628c10c1f3dcd6743e7f
pablo torres <pablotorres@MacBook-Air-de-pablo.local>
2021-12-14 16:54:17 -0300

    chore(apps/locations): add in model  country is active

--- hackaton/apps/locations/models.py
+++ hackaton/apps/locations/models.py
@@ -8,6 +8,7 @@ class Country(models.Model):
     name = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True, auto_now=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.code}: {self.name}'
