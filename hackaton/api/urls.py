90023f0875815384b76c38a6f0ccfa23474e56a2
pablo torres <pablotorres@MacBook-Air-de-pablo.local>
2021-12-14 19:49:58 -0300

    chore(api/urls): add endpoint for communes

--- hackaton/api/urls.py
+++ hackaton/api/urls.py
@@ -2,10 +2,12 @@ from rest_framework import routers
 
 # Views
 from hackaton.api.v100.locations.views.country import CountryViewset
+from hackaton.api.v100.locations.views.comuna import ComunaViewSet
 
 # Endpoint
 router = routers.SimpleRouter()
 router.register(r"v100/locations/country", CountryViewset, basename="v100-locations")
+router.register(r"v100/locations/comuna", ComunaViewSet, basename="v100-locations")
 
 
 urlpatterns = router.urls
