bdda2cb0d62c44c17698c198562cca50bf8955a1
Fabian Aravena <faravena@talana.com>
2021-12-15 17:10:30 -0300

    Migraciones Fix y Admins

--- hackaton/apps/business/models.py
+++ hackaton/apps/business/models.py
@@ -50,21 +50,6 @@ class CompanyType(models.Model):
         db_table = "company_type"
 
 
-class CompanyType(models.Model):
-    """Tipo de Razón Social"""
-
-    code = models.CharField(max_length=100, db_index=True)
-    name = models.CharField(max_length=300)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(null=True, auto_now=True)
-
-    def __str__(self):
-        return f"{self.code}: {self.name}"
-
-    class Meta:
-        db_table = "company_type"
-
-
 class Company(models.Model):
     """Razón Social"""
 
