90fa8f4c25a0ab63085ef3ed06dd02eee382ad87
Fabian Aravena <faravena@talana.com>
2021-12-15 12:47:14 -0300

    Template local

--- hackaton/settings/base.py
+++ hackaton/settings/base.py
@@ -10,17 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0vd(!slt-!40w^w2gvwmp7cy^sun1zx^18s44(%)_@m3xb52@2"
+SECRET_KEY = "boLrE8LWmIQgXCPZwAvwU8QbrV8hC0AJpntmIEKY338gN8Id"
 
 
 # Application definition
@@ -41,6 +40,7 @@ INSTALLED_APPS = [
     "hackaton.apps.users",
     "hackaton.apps.central",
     "hackaton.apps.forms",
+    "hackaton.frontend",
 ]
 
 REST_FRAMEWORK = {
